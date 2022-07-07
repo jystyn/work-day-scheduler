465e5777140f61e173bcf3ff1f5c48a090f4486f
joostyn <justyn.helgeson@gmail.com>
2022-07-07 18:34:20 -0500

    Saved input values to storage and pulls them on page load

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,15 @@
 //This variable allows us to pull the current hour from moment.js
 //H allows us to use military time
+
 var now = parseInt(moment().format('H'));
+
+//This function displays the current date on the top of the page
+function displayTime() {
+    $('#currentDay').text(moment().format('MMMM Do YYYY'));
+};
+
+setInterval(displayTime, 1000);
+displayTime();
 
 //This color codes the time blocks based on time of day
 $.each($(`:input`), function () {
@@ -14,10 +23,25 @@ $.each($(`:input`), function () {
     }
 })
 
-//This function displays the current date on the top of the page
-function displayTime() {
-    $('#currentDay').text(moment().format('MMMM Do YYYY'));
-};
+//save button saves time block inputs into local storage
+$(`.saveBtn`).on("click", function (event){
+    var timeBlockText = event.target.previousElementSibling.value;
+    var timeBlockNumber = event.target.previousElementSibling.getAttribute('id');
+    
+    localStorage.setItem(timeBlockNumber, timeBlockText);
+});
 
-setInterval(displayTime, 1000);
-displayTime();
+//This function pulls the saved inputs from local storage on page load
+function fillTimeBlocks ()  {
+    $(`#9`).val(localStorage.getItem('9'));
+    $(`#10`).val(localStorage.getItem('10')); 
+    $(`#11`).val(localStorage.getItem('11')); 
+    $(`#12`).val(localStorage.getItem('12')); 
+    $(`#13`).val(localStorage.getItem('13')); 
+    $(`#14`).val(localStorage.getItem('14'));
+    $(`#15`).val(localStorage.getItem('15'));
+    $(`#16`).val(localStorage.getItem('16'));
+    $(`#17`).val(localStorage.getItem('17'));   
+}
+
+fillTimeBlocks();

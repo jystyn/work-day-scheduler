7d595b37bfc53487e81e04e2761a214a47a210d3
joostyn <justyn.helgeson@gmail.com>
2022-07-07 17:36:22 -0500

    Added time to top of page on deployment, added color coding to time blocks

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+
+//This variable allows us to pull the current hour from moment.js
+//H allows us to use military time
+var now = parseInt(moment().format('H'));
+
+//This color codes the time blocks based on time of day
+$.each($(`:input`), function () {
+    var time = parseInt($(this).attr('id'));
+    if (time === now) {
+        $(this).addClass('present');
+    } else if (time < now) {
+        $(this).addClass('past');
+    } else if (time > now) {
+        $(this).addClass('future');
+    }
+})
+
+//This function displays the current date on the top of the page
+function displayTime() {
+    $('#currentDay').text(moment().format('MMMM Do YYYY'));
+};
+
+setInterval(displayTime, 1000);
+displayTime();
+
+
